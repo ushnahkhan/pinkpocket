47270982f86dde8525dacacc93a69f4523ce1fbe
Ushnah Khan <ushikhan09@gmail.com>
2026-05-09 20:52:30 +0500

    fixed frontend api

--- PinkPocket-backend/server.js
+++ PinkPocket-backend/server.js
@@ -10,7 +10,7 @@ const PORT=process.env.PORT||5000;
 
 connectDB();
 app.use(cors({
-    origin: "https://pinkpocket.vercel.app/",
+    origin: "https://pinkpocket.vercel.app",
     credentials: true
 }));
 app.use(express.json());
