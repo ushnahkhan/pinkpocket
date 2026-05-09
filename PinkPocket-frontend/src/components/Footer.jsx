21a0be32ba4730dff9608c2dc76070e905ec59c2
Ushnah Khan <ushikhan09@gmail.com>
2026-05-09 21:28:20 +0500

    fixed name errors

--- PinkPocket-frontend/src/components/Footer.jsx
+++ PinkPocket-frontend/src/components/Footer.jsx
@@ -10,15 +10,15 @@ const Footer = () => {
 
             <div className="footer-top">
                 <img src={pinkpicon} className="footer-logo" alt="PinkPocket"></img>
-                <p className="footer-title">theSoftGirlStore</p>
+                <p className="footer-title">PinkPocket</p>
             </div>
             <div className="footer-info">
                 <div className="footer-info-title">About Us</div>
-                <div className="footer-info-rows-text">theSoftGirlStore is a thoughtfully designed online space created
+                <div className="footer-info-rows-text">PinkPocket is a thoughtfully designed online space created
                     for young women who love aesthetics without stretching their budget. 
                     We believe that shopping for stationery and accessories should feel personal, 
                     inspiring, and fun—just like scrolling through your favorite Pinterest boards. 
-                    From soft pastel themes to customizable touches, every detail of theSoftGirlStore is crafted 
+                    From soft pastel themes to customizable touches, every detail of PinkPocket is crafted 
                     to reflect individuality and creativity. Our goal is to make stylish, affordable products 
                     accessible while delivering a smooth and enjoyable digital experience that feels uniquely yours.
                 </div>
